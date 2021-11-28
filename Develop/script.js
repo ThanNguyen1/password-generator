00e746cf8c71381cce09c636f372d6a572c5cd05
ThanNguyen1 <than.nnguyen@yahoo.com>
2021-11-28 13:43:08 -0600

    defined generatePassword() function and added psudocode to outline criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,19 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  console.log("Hey you clicked the button");
+// 1. prompts user for password criteria
+
+//    a. password length 8 > 128 characters
+//    b. lowercase, uppercase, numbers, special characters
+// 2. Validate the input.
+// 3. Generate password based on inputs
+// 4. Display password to the page. 
+  return "Password Placeholder";
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -15,3 +26,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// writePassword();
--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 ## Goals
 When user clicks on the button, the password generatator  will prompt for the following:
-* Length of the password
+* Asking for the length of the password.
 * Confirm wheather or not to include lowercase, uppercase, numeric and/or special characters.
-* After all prompts are answered, then a password is generated that matches the selected criteria.
-* Password will be displayed to user. 
+
+After all prompts are answered a password is generated that matches the selected criteria. The password will be displayed to user. 
+
+## Steps from starter code
+1. define generatePassword() function.
+2. lined out criteria and steps in psudeocode. 
